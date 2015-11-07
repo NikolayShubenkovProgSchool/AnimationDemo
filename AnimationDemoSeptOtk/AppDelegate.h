d8a8cbeb373e325bc76e1674908a7eebaabf227a
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2015-11-07 12:13:13 +0300

    Initial Commit

---
+++ AnimationDemoSeptOtk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemoSeptOtk
+//
+//  Created by Nikolay Shubenkov on 07/11/15.
+//  Copyright © 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDemoSeptOtk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimationDemoSeptOtk
+//
+//  Created by Nikolay Shubenkov on 07/11/15.
+//  Copyright © 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
